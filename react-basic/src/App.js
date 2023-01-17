141b1972aac31fd7ff9f894e2ba2902397c551fa
EllaSEON <shineunhye5@gmail.com>
2023-01-17 16:14:09 +0900

    블로그 포스트 생성 폼 만들기

---
+++ react-basic/src/App.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const onSubmit = () => {
+    axios.post("http://localhost:3001/posts", {
+      title: title,
+      body: body,
+    });
+  };
+  return (
+    <div className="container">
+      <div className="mb-3">
+        <label className="form-label">Title</label>
+        <input
+          className="form-control"
+          type="text"
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Body</label>
+        <textarea
+          className="form-control"
+          type="text"
+          value={body}
+          onChange={(event) => {
+            setBody(event.target.value);
+          }}
+          rows="20"
+        />
+      </div>
+      <button className="btn btn-primary" onClick={onSubmit}>
+        Post
+      </button>
+    </div>
+  );
+}
+
+export default App;
