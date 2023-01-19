d0a3c45ef3323ec69deb2e1e23e4fb85e2d77e3d
EllaSEON <shineunhye5@gmail.com>
2023-01-19 16:14:33 +0900

    <feat> : 블로그 리스트 get 통신

--- react-basic/src/Pages/ListPage.jsx
+++ react-basic/src/Pages/ListPage.jsx
@@ -1,4 +1,28 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const ListPage = () => {
-  return <div>ListPage</div>;
+  const [posts, setPosts] = useState([]);
+  const getPosts = () => {
+    axios.get("http://localhost:3001/posts").then((res) => {
+      setPosts(res.data);
+    });
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+  return (
+    <div>
+      <h1>Blogs</h1>
+      {posts.map((post) => {
+        return (
+          <div className="card mb-3" key={post.id}>
+            <div className="card-body">{post.title}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 export default ListPage;
