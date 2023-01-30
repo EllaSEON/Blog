e9fd614a289bed835c6159ed0ec68fb31e068b6e
EllaSEON <shineunhye5@gmail.com>
2023-01-30 15:27:15 +0900

    <feat> : Card 컴포넌트 props 타입체크

--- react-basic/src/Pages/ListPage.jsx
+++ react-basic/src/Pages/ListPage.jsx
@@ -18,10 +18,9 @@ const ListPage = () => {
       <h1>Blogs</h1>
       {posts.map((post) => {
         return (
-          <Card key={post.id} title={post.title} />
-          // <div className="card mb-3" key={post.id}>
-          //   <div className="card-body">{post.title}</div>
-          // </div>
+          <Card key={post.id} title={post.title}>
+            <button>button</button>
+          </Card>
         );
       })}
     </div>
--- react-basic/src/components/Card.jsx
+++ react-basic/src/components/Card.jsx
@@ -1,8 +1,29 @@
-const Card = ({ title }) => {
+import PropTypes from "prop-types";
+
+const Card = ({ title, children }) => {
   return (
     <div className="card mb-3">
-      <div className="card-body">{title}</div>
+      <div className="card-body">
+        <div className="d-flex justify-content-between">
+          <div>{title}</div>
+          {children && <div>{children}</div>}
+        </div>
+      </div>
     </div>
   );
 };
+
+// 컴포넌트의 props에서 타입을 체크하고 싶다면 propTypes 속성을 할당할 수 있음
+//prop에 유효하지 않은 값이 전달되면 자바스크립트 콘솔에 경고가 노출됨. 성능상의 이유로 propTypes는 개발모드에서만 체크한다
+// https://reactjs-kr.firebaseapp.com/docs/typechecking-with-proptypes.html
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.element, // 한개의 컴포넌트만 자식으로 설정가능
+};
+
+// prop가 아무것도 없을때 기본값 설정가능
+Card.defaultProps = {
+  children: null,
+};
+
 export default Card;
