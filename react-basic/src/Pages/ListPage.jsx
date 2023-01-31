fea036950feb8b73c8825f5ed2fcb53d28916667
EllaSEON <shineunhye5@gmail.com>
2023-01-31 11:39:31 +0900

    <feat> : 블로그 포스트 삭제기능

--- react-basic/src/Pages/ListPage.jsx
+++ react-basic/src/Pages/ListPage.jsx
@@ -6,6 +6,8 @@ import { Link, useNavigate } from "react-router-dom";
 const ListPage = () => {
   const navigate = useNavigate();
   const [posts, setPosts] = useState([]);
+
+  // 게시물 보여주기
   const getPosts = () => {
     axios.get("http://localhost:3001/posts").then((res) => {
       setPosts(res.data);
@@ -15,6 +17,17 @@ const ListPage = () => {
   useEffect(() => {
     getPosts();
   }, []);
+
+  // 삭제 버튼
+  const deleteBlog = (e, id) => {
+    e.stopPropagation();
+    axios.delete(`http://localhost:3001/posts/${id}`).then(() => {
+      setPosts((prevPosts) => {
+        return prevPosts.filter((post) => post.id !== id);
+        // 화면에서도 삭제된걸 보여주기 위해서 설정
+      });
+    });
+  };
   return (
     <div>
       <div className="d-flex justify-content-between">
@@ -31,7 +44,14 @@ const ListPage = () => {
             key={post.id}
             title={post.title}
             onClick={() => navigate("/blogs/edit")}
-          ></Card>
+          >
+            <div
+              className="btn btn-danger btn-sm"
+              onClick={(e) => deleteBlog(e, post.id)}
+            >
+              Delete
+            </div>
+          </Card>
         );
       })}
     </div>
