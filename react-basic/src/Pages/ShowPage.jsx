e5ad1d233c89ff9d742198904aedbc6b3c5a8313
EllaSEON <shineunhye5@gmail.com>
2023-02-01 09:38:35 +0900

    <feat> : 블로그 디테일 페이지 화면에 보이기

--- react-basic/src/Pages/ShowPage.jsx
+++ react-basic/src/Pages/ShowPage.jsx
@@ -1,8 +1,35 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const ShowPage = () => {
   const { id } = useParams();
-  console.log(id);
-  return <div>show page</div>;
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const getPost = (id) => {
+    axios.get(`http://localhost:3001/posts/${id}`).then((response) => {
+      setPost(response.data);
+      setLoading(false);
+    });
+  };
+
+  // 한번만 실행시킬 것이기 때문에 []
+  useEffect(() => {
+    getPost(id);
+  }, []);
+
+  //로딩 스피너
+  if (loading) {
+    return <LoadingSpinner />;
+  } else {
+    return (
+      <div>
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
+      </div>
+    );
+  }
 };
 export default ShowPage;
